67dd55bc7dffeabd73d99a16116167cf499bc74a
John Behnke <john@behnke.xyz>
2022-12-03 01:18:39 -0500

    Day 3, Swift is weird wtf

---
+++ 2022/Day 03/problem.swift
@@ -0,0 +1,52 @@
+//
+//  problem.swift
+//  
+//
+//  Created by John Behnke on 12/3/22.
+//
+
+import Foundation
+
+
+extension Array {
+  func chunked(into size: Int) -> [[Element]] {
+      return stride(from: 0, to: count, by: size).map {
+          Array(self[$0 ..< Swift.min($0 + size, count)])
+      }
+  }
+}
+
+func readFile(fileName: String) -> [String]{
+  let pathToFile = FileManager.default.currentDirectoryPath + "/"
+  let content = try! String(contentsOfFile: pathToFile + fileName, encoding: .utf8)
+  return Array(content.split(separator: "\n").map{String($0)})
+}
+
+func problem1(rucksacks: [String]) -> Int {
+  var sum = 0
+  for rucksack in rucksacks {
+    let l = rucksack[..<rucksack.index(rucksack.startIndex, offsetBy: rucksack.count / 2)]
+    let r = rucksack[rucksack.index(rucksack.endIndex, offsetBy: rucksack.count / 2 * -1)...]
+    let common = Set(l).intersection(Set(r))
+    let chars = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    sum += chars.firstIndex(of: common.first!)!.utf16Offset(in: chars)
+  }
+return sum
+
+}
+
+func problem2(rucksacks: [String]) -> Int {
+  var sum = 0
+  for collection in rucksacks.chunked(into: 3) {
+    let one = Set(collection[0])
+    let two = Set(collection[1])
+    let three = Set(collection[2])
+    let common = one.intersection(two).intersection(three)
+    let chars = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    sum += chars.firstIndex(of: common.first!)!.utf16Offset(in: chars)
+  }
+  return sum
+}
+let input = readFile(fileName: CommandLine.arguments[1])
+print(problem1(rucksacks: input))
+print(problem2(rucksacks: input))
